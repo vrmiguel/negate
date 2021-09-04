42862ded00f4d50f12bcd185b8e4b234eab3da8d
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-09-03 22:54:02 -0300

    Add docstring for `negate`, add CI workflow

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,39 @@ use syn::{spanned::Spanned, Item};
 
 use crate::generator::gen_negated_function;
 
+
+/// The [`negate`](crate::negate) attribute macro creates a new function that _negates_ the function it was given.
+/// For example, if applied to a function titled `is_student`, the macro will create a
+/// new function named `is_not_student`, which simply negates the previous function.
+///
+/// ```rust
+/// use negate::negate;
+///
+/// #[negate] // <- negate will implement a `is_not_even` function!
+/// pub fn is_even(x: i32) -> bool {
+///     x % 2 == 0
+/// }
+///
+/// // We generated `is_not_even`!
+/// assert!(is_not_even(5));
+///
+/// struct Word(&'static str);
+///
+/// impl Word {
+///     pub fn new(word: &'static str) -> Self {
+///         Self (word)
+///     }
+///
+///     #[negate] // <- negate will implement a `is_not_uppercase` function!
+///     pub fn is_uppercase(&self) -> bool {
+///         self.0 == self.0.to_uppercase()
+///     }
+/// }
+/// let my_name = Word::new("My Name");
+///
+/// // We generated `is_not_uppercase`!
+/// assert!(my_name.is_not_uppercase());
+/// ```
 #[proc_macro_attribute]
 pub fn negate(_: TokenStream, input: TokenStream) -> TokenStream {
     let item: syn::Item = syn::parse(input).expect("failed to parse input into `syn::Item`");
